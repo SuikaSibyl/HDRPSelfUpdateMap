db3711647dd46345650f12ed8cf5d164aa3ffbcc
SuikaSibyl <60009633+SuikaSibyl@users.noreply.github.com>
2021-07-19 20:35:48 +0800

    size changable

--- Assets/SampleSceneAssets/TutorialInfo/ExploreMapToolkit/Scripts/ExploreMapManager.cs
+++ Assets/SampleSceneAssets/TutorialInfo/ExploreMapToolkit/Scripts/ExploreMapManager.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ExploreMapManager : MonoBehaviour
 {
     public Camera TrackerCamera;
     public Transform TrackedPlayer;
     public Transform PlayerIndicator;
+    public float ExploreRadius = 1;
 
+    ParticleSystem trajectory;
+
+    public void StartSystem()
+    {
+        trajectory.Play(true);
+    }
+
+    public void DisableSystem()
+    {
+        trajectory.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+    }
+
+    public void SetExploreRadius(float radius)
+    {
+        ExploreRadius = radius;
+        PlayerIndicator.localScale = new Vector3(radius, radius, radius);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        TrackerCamera.clearFlags = CameraClearFlags.Nothing;
+        trajectory = PlayerIndicator.GetChild(0).GetComponent<ParticleSystem>();
+        SetExploreRadius(2);
     }
 
     void FixedUpdate()
